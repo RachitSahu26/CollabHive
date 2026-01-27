762d142c18925a35c60393f26183f132d541157f
Rachit Sahu <curiousrachit.26@gmail.com>
2026-01-27 20:39:26 +0530

    Initial project setup with frontend and backend structure

---
+++ Backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+// import cors from "cors";
+import { ENV } from "./config/env.js";
+
+
+
+const app = express();
+
+// app.use(cors({ origin: process.env.CLIENT_URL }));
+
+app.listen(ENV.PORT, () =>
+  console.log('Server running on port',ENV.PORT)
+);
