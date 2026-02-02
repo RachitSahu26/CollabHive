9ed757bf985006049d2f50abada9ce80e1b0b6ac
Rachit Sahu <curiousrachit.26@gmail.com>
2026-02-02 13:31:34 +0530

    feat: update server configuration and startup logic

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 // import cors from "cors";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 
 
@@ -8,6 +9,9 @@ const app = express();
 
 // app.use(cors({ origin: process.env.CLIENT_URL }));
 
-app.listen(ENV.PORT, () =>
-  console.log('Server running on port',ENV.PORT)
+app.listen(ENV.PORT, () =>{
+console.log('Server running on port',ENV.PORT)
+    connectDB()
+}
+  
 );
